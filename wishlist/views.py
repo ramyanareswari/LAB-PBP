90195657e6ec7e3d5c397a3eec272a6e7193316a
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-08 00:57:55 +0700

    Lab 1 wishlist app done

--- wishlist/views.py
+++ wishlist/views.py
@@ -5,8 +5,11 @@ from wishlist.models import BarangWishlist
 # Sebuah fungsi yang menerima parameter request dan mengembalikan render(request, "wishlist.html")
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
+    # name_header = Name.objects.all()
+    
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': 'Kak Cinoy'
+        'nama':  'Ramya Nareswari'
     }
+
     return render(request, "wishlist.html", context)
