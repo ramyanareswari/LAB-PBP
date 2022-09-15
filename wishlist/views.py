41467406904f12cfc3e17eff28b788a48f777e3b
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-09-15 08:12:39 +0700

    Lab 2 done

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,7 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
-
 
 # Sebuah fungsi yang menerima parameter request dan mengembalikan render(request, "wishlist.html")
 def show_wishlist(request):
@@ -13,3 +14,21 @@ def show_wishlist(request):
     }
 
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+## Return Data by ID
+def show_json_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
