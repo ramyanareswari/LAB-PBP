e3af976af6dd60302850f34eaab442cc3545a45c
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-10-03 14:22:18 +0700

    Edited admin.py

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
-# Register your models here.
-user = User.objects.create_user('ramyanares', 'ramya.nareswari@ui.ac.id','lollipop31')
-user.save()
+from django.contrib import admin
+from .models import *
+
+admin.site.register(BarangWishlist)
