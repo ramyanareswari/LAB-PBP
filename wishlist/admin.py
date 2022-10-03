a843ef20790199224844fa63257ef00a3ced6d57
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-10-03 14:10:52 +0700

    Created superuser

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,8 +1,3 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-
 from django.contrib.auth.models import User
 # Register your models here.
 user = User.objects.create_user('ramyanares', 'ramya.nareswari@ui.ac.id','lollipop31')
